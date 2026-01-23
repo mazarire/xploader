3439c4bf76ed18477fe5ecad7a930c941c88a123
anonymousofficial26 <t.0717237343@gmail.com>
2026-01-23 09:43:56 +0200

    Update startBot.js

--- bot/startBot.js
+++ bot/startBot.js
@@ -1,34 +1,17 @@
-import makeWASocket, {
-  useMultiFileAuthState,
-  fetchLatestBaileysVersion
-} from "@whiskeysockets/baileys"
-import loadPlugins from "./loader.js"
+import makeWASocket from "@whiskeysockets/baileys"
+import { io } from "../web/server.js"
 
 export async function startBot(id, config) {
-  const { state, saveCreds } =
-    await useMultiFileAuthState(`./sessions/${id}`)
-  const { version } = await fetchLatestBaileysVersion()
+  const sock = makeWASocket({ /* your options */ })
 
-  const sock = makeWASocket({
-    auth: state,
-    version,
-    printQRInTerminal: false
-  })
-
-  sock.ev.on("creds.update", saveCreds)
-
-  const plugins = await loadPlugins()
-
-  sock.ev.on("connection.update", u => {
-    if (u.qr) console.log(`[${id}] QR RECEIVED`)
-  })
-
-  sock.ev.on("messages.upsert", async ({ messages }) => {
-    const msg = messages[0]
-    if (!msg?.message) return
-    for (const plugin of plugins)
-      await plugin(sock, msg, config)
+  sock.ev.on("connection.update", update => {
+    if (update.qr) {
+      console.log(`[${id}] QR RECEIVED`)
+      io.emit("qr", update.qr)
+    }
   })
 
   return sock
+}
+
 }
